7f6be4ef50db26b51f20a40899cee60ffe870be0
Muhammad <muhammad.ppqita@gmail.com>
2023-10-25 11:17:45 +0700

    update

--- src/pages/api/hello.js
+++ src/pages/api/hello.js
@@ -1,5 +1,11 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
 
 export default function handler(req, res) {
-  res.status(200).json({ name: 'John Doe' })
+ console.log(req.method);
+ console.log(req.body);
+  if (req.method !== 'POST') {
+     res.status(200).json({ name: "Jhon Doe" });
+  } else {
+    
+  }
 }
